42fabd462e94a284c797b61e6b5e3522eda2d387
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2019-11-06 07:06:24 +0900

    edit prod.py

--- conf/settings/prod.py
+++ conf/settings/prod.py
@@ -11,6 +11,7 @@ DATABASES = {
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_ALLOW_WHITELIST = [
+    'localhost:8000'
     'localhost:3000' #나중에 now 링크 쓰기
     'https://veeto-cli.gywls517.now.sh:3000'
 ]
