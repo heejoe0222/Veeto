7681a52ca9112594c068d547d4bb227d589c4209
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2019-12-01 04:16:37 +0900

    re-upload main serializer

---
+++ main/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Room
+from accounts.serializers import UserInfoSerializer
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    members = UserInfoSerializer(read_only=True, many=True)
+    date = serializers.SerializerMethodField()
+    time = serializers.SerializerMethodField()
+    activity = serializers.SerializerMethodField()
+    class Meta:
+        model = Room
+        exclude = ('roomCandidate',)
+    def get_date(self,obj):
+        return obj.roomCandidate.date
+    def get_time(self,obj):
+        return obj.roomCandidate.time
+    def get_activity(self,obj):
+        return obj.roomCandidate.activity.id
