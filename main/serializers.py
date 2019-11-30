59f9ad24388a6f1cfe3aaf1b4f8820ae5f2bdcf9
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2019-12-01 04:06:59 +0900

    edit RoomSerializer.py

--- main/serializers.py
+++ main/serializers.py
@@ -30,40 +30,5 @@ class RoomSerializer(serializers.ModelSerializer):
         return obj.roomCandidate.activity.id
 
 
-'''
-class SimpleRoomSerializer(serializers.ModelSerializer):
-    year = serializers.SerializerMethodField()
-    month = serializers.SerializerMethodField()
-    day = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Room
-        exclude = ('members', 'date', 'total_number_of_members')
-
-    def get_year(self,obj):
-        return obj.date.year
-
-    def get_month(self,obj):
-        return obj.date.month
-
-    def get_day(selfs,obj):
-        return obj.date.day
-
-    def get_place(self,obj):
-        return obj.place.name
-
-
-class ActivityPlaceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ActivityPlace
-        exclude = ('activity',)
-
-
-class RoomUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RoomUser
-        fields = '__all__'
-'''
-
 
 
