7e3d630791715e99092595b1af31076234539376
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2019-12-02 13:51:50 +0900

    Change CORS setting to False and allow_whitelist

--- conf/settings/prod.py
+++ conf/settings/prod.py
@@ -9,9 +9,7 @@ DATABASES = {
     'default': secrets['DB_SETTINGS']['PRODUCTION']
 }
 
-CORS_ORIGIN_ALLOW_ALL = True  # 일단 TRUE -> 나중에 바꾸기
+CORS_ORIGIN_ALLOW_ALL = False  # 일단 TRUE -> 나중에 바꾸기
 CORS_ORIGIN_ALLOW_WHITELIST = [
-    'http://localhost:8000',
-    'http://localhost:3000', #나중에 now 링크 쓰기
     'https://veeto-cli.gywls517.now.sh',
 ]
